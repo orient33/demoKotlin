3882da2062161d2705b175851cbd8319cd5eb83b
dfdun <498505698@qq.com>
2023-02-24 14:30:15 +0800

    miniSdk 26, 删除一些资源等 ,适配android13带theme的icon-monochrome

--- app/src/main/java/com/example/appinfo/VH.kt
+++ app/src/main/java/com/example/appinfo/VH.kt
@@ -2,13 +2,12 @@ package com.example.appinfo
 
 import android.annotation.SuppressLint
 import android.content.pm.LauncherActivityInfo
-import androidx.recyclerview.widget.RecyclerView
-import android.widget.TextView
 import android.content.pm.PackageInfo
 import android.graphics.drawable.Drawable
-import android.os.Build
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.example.appName
 import com.example.kotlindemo.R
 
@@ -23,15 +22,13 @@ class VH internal constructor(v: View) : RecyclerView.ViewHolder(v) {
         }
 
     @SuppressLint("SetTextI18n")
-    fun bindPackageInfo(pos:Int, listener: View.OnClickListener?, info: PackageInfo) {
+    fun bindPackageInfo(pos: Int, listener: View.OnClickListener?, info: PackageInfo) {
         val title = info.applicationInfo.packageName
         val d = info.applicationInfo.loadIcon(itemView.context.packageManager)
         icon.setImageDrawable(d)
         name.text = "${1 + pos}) ${info.appName}, ${d.javaClass.simpleName}"
         cn.text = "$title \n targetSdk: ${info.applicationInfo.targetSdkVersion}"
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            cn.append(", minSdk: ${info.applicationInfo.minSdkVersion}, uid:${info.applicationInfo.uid},vCode:${info.versionCode},vName:${info.versionName}")
-        }
+        cn.append(", minSdk: ${info.applicationInfo.minSdkVersion}, uid:${info.applicationInfo.uid},vCode:${info.versionCode},vName:${info.versionName}")
         itemView.tag = title
         itemView.setOnClickListener(listener)
     }
@@ -42,7 +39,7 @@ class VH internal constructor(v: View) : RecyclerView.ViewHolder(v) {
         val cnn: String = info.componentName.toShortString()
         icon.setImageDrawable(drawable)
         name.text = (1 + pos).toString() + ")  " + info.label
-        cn.text = cnn +"\n" + drawable.javaClass.simpleName
+        cn.text = cnn + "\n" + drawable.javaClass.simpleName
         itemView.setOnClickListener(listener)
     }
 }
