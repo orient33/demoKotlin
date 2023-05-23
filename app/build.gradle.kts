17cd899aff59dd5161f26c97b3b8324a4ee87ea3
dfdun <498505698@qq.com>
2023-05-23 11:10:28 +0800

    update sdk library version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,33 +43,30 @@ android {
 
 dependencies {
 //    implementation(fileTree(include:['*.jar'], dir: 'libs'))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.10")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.20")
     implementation("androidx.window:window:1.0.0")
-    implementation("androidx.core:core-splashscreen:1.0.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.fragment:fragment-ktx:1.5.5")
+    implementation("androidx.fragment:fragment-ktx:1.5.7")
     implementation("androidx.preference:preference-ktx:1.2.0")
     implementation("androidx.palette:palette-ktx:1.0.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.material:material:1.8.0")
-    implementation("androidx.annotation:annotation:1.5.0")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.annotation:annotation:1.6.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.0")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
-
-    implementation("androidx.room:room-runtime:2.5.0")
-    kapt("androidx.room:room-compiler:2.5.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
-//    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.room:room-runtime:2.5.1")
+    kapt("androidx.room:room-compiler:2.5.1")
     // ViewModel and LiveData
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 //    kapt("androidx.lifecycle:lifecycle-compiler:2.2.0")
     // alternately - if using Java8, use the following instead of compiler
-    implementation("androidx.lifecycle:lifecycle-common-java8:2.5.1")
-
-    implementation("androidx.lifecycle:lifecycle-livedata-core-ktx:2.5.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-core-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
 
     implementation("androidx.media:media:1.6.0")
 //    val media2_version = "1.2.1"
@@ -89,9 +86,9 @@ dependencies {
     api("com.squareup.retrofit2:converter-gson:2.9.0")
     api("com.squareup.okhttp3:okhttp:4.10.0")
     api("com.squareup.okhttp3:logging-interceptor:4.10.0")
-    api("com.google.android.exoplayer:exoplayer-core:2.18.1")
+    api("com.google.android.exoplayer:exoplayer-core:2.18.7")
 
-    implementation("com.airbnb.android:lottie:4.0.0") //support包用2.7.0
+    implementation("com.airbnb.android:lottie:5.0.3") //support包用2.7.0
     implementation("pub.devrel:easypermissions:3.0.0")//非androidX时候使用版本 2.0.1
     //方便调试,查看数据库的.可以在PC上直接浏览db,,不需要每次pull出来文件.
 //    debugImplementation ("com.amitshekhar.android:debug-db:1.0.6")
